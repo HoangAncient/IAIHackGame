dd23dc9d9c1528dee18e910873bb364672650b99
Quachday <100117943+Quachday@users.noreply.github.com>
2023-07-26 15:09:16 +0700

    character main game

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -8,7 +8,7 @@ public class StartMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
+        SceneManager.LoadScene("Scenes/Gamedemo");  
     }
 
 }
